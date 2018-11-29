7d13ec58f1e299b75e3c2de85f63af0cc35c865e
Alejandro <Alejandrehl@icloud.com>
2018-11-29 17:07:59 -0300

    Solo falta agregar bien firebase al proyecto por npm

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="!username">
+      You can't chat without a name. What's your name? <br />
+      <input type="text" placeholder="Name" v-on:keyup.enter="updateUsername">
+    </div>
+    <div v-else>
+      <div>
+        From : {{username}}<br />
+        Message:<br />
+        <textarea name="" id="" cols="30" rows="10" placeholder="New Message" v-on:keyup.enter="sendMessage">
+
+        </textarea>
+      </div>
+      <hr>
+      <div class="messages">
+        <h3>Messages</h3>
+        <div class="message" v-for="message in messages">
+          <strong>{{message.username}}</strong>
+          <p>{{message.text}}</p>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import fire from './fire'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      username: '',
+      messages: []
+    }
+  },
+  methods: {
+    updateUsername(e) {
+      e.preventDefault();
+      if(e.target.value){
+        this.username = e.target.value;
+      }
+    },
+    sendMessage(e) {
+      e.preventDefault();
+      if(e.target.value){
+
+        const message = {
+          username: this.username,
+          text: e.target.value
+        }
+        fire.database().ref('messages').push(message);
+        e.target.value = ''
+      }
+    }
+  },
+  mounted(){
+    let vm = this;
+    const itemsRef = fire.database().ref('messages');
+    itemsRef.on('value', snapshot => {
+      let data = snapshot.val();
+      let messages = [];
+      Object.keys(data).forEach(key => {
+        messages.push({
+          id: key,
+          username: data[key].username,
+          text: data[key].text
+        });
+      });
+      vm.messages = messages;
+    })
   }
 }
 </script>
@@ -24,5 +82,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.messages {
+  text-align: left;
+}
+.message {
+  border: #000 solid 2px;
+  padding: 5px;
+  margin: 5px;
+  width: 200px;
 }
 </style>
